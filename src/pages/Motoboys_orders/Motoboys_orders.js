100c310bad61863a863ee11af260c23d31239e7d
Cledson <cledson1996@gmail.com>
2023-02-02 18:00:52 -0300

    ficando show

--- src/pages/Motoboys_orders/Motoboys_orders.js
+++ src/pages/Motoboys_orders/Motoboys_orders.js
@@ -1,11 +1,12 @@
 import Headers from "../../components/headers/headers"
 import Navbar from "../../components/navbar/navbar"
 import { useEffect, useState } from 'react';
-import { getUsers,getMotoboys } from "../../request/request";
+import { getUsers, getMotoboys, getOrders_filter } from "../../request/request";
 import { useNavigate } from 'react-router-dom'
 import DatePicker from "react-datepicker";
 import pt from 'date-fns/locale/pt';
-import { Chart } from "react-google-charts";
+import moment from "moment";
+import MUIDataTable from "mui-datatables";
 
 
 export default function Motoboys_orders() {
@@ -13,13 +14,39 @@ export default function Motoboys_orders() {
     const navigate = useNavigate();
     const token = localStorage.getItem("token");
 
-    const [data, setdata] = useState({  });
+    const [data, setdata] = useState({});
+    const [table, settable] = useState(true);
+    const [table1, settable1] = useState(false);
+    const [table2, settable2] = useState(false);
     const [user, setuser] = useState("loading");
     const [navbar, setnavbar] = useState(true)
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setendDate] = useState(new Date());
     const [motoboys, setmotoboys] = useState([]);
+    const [QTD_orders, setQTD_orders] = useState(0);
+    const [orders, setorders] = useState([])
+    const [qtdausentes, setqtdausentes] = useState(0);
+    const [qtdclients, setqtdclients] = useState(0);
+    const [motoboy, setmotoboy] = useState(0);
+    const [orderstd, setorderstd] = useState(0)
 
+    const columns = ["Cliente", "Qtd pedidos", "QTD ausentes", "pedidos liquido"];
+    const columnsone = ["Cliente", "Numero pedido", "Qtd", "Situação", "Imagem", "Data"];
+    const columnstwo = ["Cliente", "Qtd pedidos", "QTD ausentes", "pedidos liquido"];
+    const options = {
+        responsive: 'scrollMaxHeight',
+        filterType: 'checkbox',
+        selectableRows: 'none',
+        customRowRender: (data, rowIndex) => {
+            return (
+                <tr key={rowIndex} >
+                    {data.map((prop, index) => {
+                        return <td className="my-custom-row-class" key={index}>{prop}</td>;
+                    })}
+                </tr>
+            );
+        }
+    };
     function handleForm({ value, name }) {
 
         setdata({
@@ -27,6 +54,46 @@ export default function Motoboys_orders() {
             [name]: value,
         });
     };
+    useEffect(() => {
+
+        const data_send = startDate.toLocaleDateString('en-US', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        })
+        const body = {
+            id: data.motoboysid || 0,
+            date: data_send,
+            dateNext: moment(endDate).add(1, 'days').format('MM-DD-YYYY')
+        }
+
+        const answer = getOrders_filter(body, token);
+        answer.then((ref) => {
+            setmotoboy(ref.data[0].motoboy)
+            console.log(ref.data)
+            let uniqueclients = Array.from(new Set(ref.data.map(obj => obj.name)));
+            setqtdausentes(ref.data.filter(ref => ref.situation === 'ausente').length)
+            setQTD_orders(ref.data.reduce((acc, obj) => acc + Number(obj.qtd), 0));
+            setqtdclients(uniqueclients);
+            console.log(uniqueclients)
+            setorderstd(ref.data.map((fifa) => {
+                return [fifa.name, fifa.number, fifa.qtd, fifa.situation, <a href={fifa.image} target="_blank" >Abrir</a>, moment(fifa.dataexit).format('DD/MM/YYYY')]
+            }))
+            setorders(uniqueclients.map((props) => {
+                let filter = ref.data.filter(rec => rec.name === props);
+                let totalqtd = filter.reduce((acc, obj) => acc + Number(obj.qtd), 0)
+                let totalaus = filter.filter(rece => rece.situation === 'ausente').length
+                let liquid = totalqtd - totalaus;
+                return [props, totalqtd, totalaus, liquid]
+            }))
+        });
+        answer.catch((ref) => {
+            if (ref.response.status === 404) {
+                navigate('/signin')
+            }
+        })
+    }, [startDate, data.motoboysid, endDate])
+
 
     useEffect(() => {
         const answer = getUsers(token);
@@ -91,15 +158,15 @@ export default function Motoboys_orders() {
 
                                     </div>
                                     <div class="form-group" style={{ minWidth: "70px", marginLeft: "35px" }}>
-                                                <label for="inputStatus">Motoboy</label>
-                                                <select name="motoboysid" id="inputStatus" class="form-control custom-select" onChange={(e) => handleForm({ name: e.target.name, value: e.target.value, })}>
-                                                    <option selected disabled>Selecione</option>
-                                                    {motoboys ? motoboys.map((ref, index) => {
-                                                        return <option value={ref.id} key={index} >{ref.name}</option>
-                                                    }) : <></>}
-                                                </select>
-                                            </div>
-                                    
+                                        <label for="inputStatus">Motoboy</label>
+                                        <select name="motoboysid" id="inputStatus" class="form-control custom-select" onChange={(e) => handleForm({ name: e.target.name, value: e.target.value, })}>
+                                            <option selected disabled>Selecione</option>
+                                            {motoboys ? motoboys.map((ref, index) => {
+                                                return <option value={ref.id} key={index} >{ref.name}</option>
+                                            }) : <></>}
+                                        </select>
+                                    </div>
+
                                 </div>
 
 
@@ -114,63 +181,95 @@ export default function Motoboys_orders() {
                     </div>
                 </div>
                 <section class="content">
-                <div class="container-fluid">
+                    <div class="container-fluid">
 
-                    <div class="row">
-                        <div class="col-lg-3 col-6">
+                        <div class="row">
+                            <div class="col-lg-3 col-6">
 
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>{1}</h3>
+                                <div class="small-box bg-info" >
+                                    <div class="inner">
+                                        <h3>{QTD_orders}</h3>
 
-                                    <p>Pedidos </p>
+                                        <p>Pedidos </p>
+                                    </div>
+                                    <div class="icon" >
+                                        <i class="ion ion-clipboard"></i>
+                                    </div>
+                                    <a onClick={() => { settable(false); settable1(true); settable2(false) }} style={{ zIndex: "0" }} class="small-box-footer" >Mais informações <i class="fas fa-arrow-circle-right"  ></i></a>
                                 </div>
-                                <div class="icon">
-                                    <i class="ion ion-clipboard"></i>
+                            </div>
+
+
+                            <div class="col-lg-3 col-6">
+
+                                <div class="small-box bg-primary">
+                                    <div class="inner">
+                                        <h3>{qtdclients.length || 0}</h3>
+
+                                        <p>Clientes utilizados</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-pie-graph"></i>
+                                    </div>
+
                                 </div>
-                                <a href="/order_day" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                            <div class="col-lg-3 col-6">
+
+                                <div class="small-box bg-danger">
+                                    <div class="inner">
+                                        <h3>{qtdausentes}</h3>
+
+                                        <p>Pedidos ausentes</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-clipboard"></i>
+                                    </div>
+                                    <a onClick={() => { settable(false); settable1(false); settable2(true) }} class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-6">
+
+                                <div class="small-box bg-warning">
+                                    <div class="inner">
+
+                                        <p>Resumo</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-clipboard"></i>
+                                    </div>
+                                    <a onClick={() => { settable(true); settable1(false); settable2(false) }} class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
+                                </div>
                             </div>
                         </div>
 
 
-                        <div class="col-lg-3 col-6">
+                        <div class="row">
+                            {table === true ?
+                                <MUIDataTable
+                                    title={`Pedidos do dia  ${moment(startDate).format('MM/DD/YYYY')} até  ${moment(endDate).format('MM/DD/YYYY')} do motoboy ${motoboy}`}
+                                    data={orders}
+                                    columns={columns}
+                                    options={options}
+                                />
+                                :
+                                table1 === true ?
+                                    <MUIDataTable
+                                        title={`Pedidos do dia  ${moment(startDate).format('MM/DD/YYYY')} até  ${moment(endDate).format('MM/DD/YYYY')} do motoboy ${motoboy}`}
+                                        data={orderstd}
+                                        columns={columnsone}
+                                        options={options}
+                                    />
+                                    :
+                                    table2 === true ?
+                                        "terceira" :
+                                        <></>}
 
-                            <div class="small-box bg-primary">
-                                <div class="inner">
-                                    <h3>{1}</h3>
 
-                                    <p>Clientes utilizados</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
                         </div>
-                        <div class="col-lg-3 col-6">
 
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>{1}</h3>
-
-                                    <p>Pedidos ausentes</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-clipboard"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
                     </div>
-
-
-                    <div class="row">
-
-
-                    </div>
-                  
-                </div>
-            </section>
+                </section>
             </div>
 
         </>
